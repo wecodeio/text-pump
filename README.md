eb346b6b512d6b34b45c9e7d50d08332ca8c5819
Lucas Videla <videla.lucas@gmail.com>
2014-10-15 00:30:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Text Pump
+
+This jQuery plugin allows you to pump your text so it fits the container
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.githubusercontent.com/wecodeio/jquery-text-pump/master/dist/jquery.text-pump.min.js
+[max]: https://raw.githubusercontent.com/wecodeio/jquery-text-pump/master/dist/jquery.text-pump.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/text-pump.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ src/text-pump.js
@@ -0,0 +1,45 @@
+/*
+ * text-pump
+ * 
+ *
+ * Copyright (c) 2014 Lucas Videla
+ * Licensed under the MIT license.
+ */
+
+ (function ($) {
+
+  $.fn.pump = function(options) {
+    var settings = $.extend({
+      ratio: 1
+    }, options );
+
+    var text = $(this);
+    var container = $(this).parent();
+
+    var parSize = container.width();
+    var curSize = text.width();
+    var lastFontSize, newFontSize;
+    var innerRatio = 0.9;
+    while (Math.abs(parSize / curSize - 1) > 0.02) {
+      lastFontSize = newFontSize;
+      var curFontSize = parseFloat(text.css('font-size'), 10);
+      var sizeRatio = parSize / curSize;
+      newFontSize = curFontSize * sizeRatio * settings.ratio * innerRatio;
+      text.css({
+        'font-size': newFontSize + 'px',
+        'line-height': newFontSize + 'px',
+        'overflow': 'visible'
+      });
+      parSize = container.width();
+      curSize = text.width();
+      innerRatio += 0.01;
+    }
+
+    text.css({
+      'font-size': lastFontSize + 'px',
+      'line-height': lastFontSize + 'px',
+      'overflow': 'visible'
+    });
+
+  }
+}(jQuery));
