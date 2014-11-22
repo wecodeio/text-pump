a689145659fb66019003db9f497fb48572f54827
Lucas Videla <videla.lucas@gmail.com>
2014-11-22 20:09:40 -0300

    New version that honors resize

--- src/jquery.text-pump.js
+++ src/jquery.text-pump.js
@@ -14,33 +14,46 @@
     }, options );
 
     var text = $(this);
-    text.css({'font-size': '1px'})
-    var container = $(this).parent();
 
-    var parSize = container.width();
-    var curSize = text.width();
-    var lastFontSize, newFontSize;
-    var innerRatio = 0.9;
-    while (parSize / curSize - 1 > 0.05) {
-      lastFontSize = newFontSize;
-      var curFontSize = parseFloat(text.css('font-size'), 10);
-      var sizeRatio = parSize / curSize;
-      newFontSize = curFontSize * sizeRatio * innerRatio;
+    $(window).on("resize",function(e){
+      pumpDo(text);
+    });
+
+    pumpDo(text);
+
+    function pumpDo(text) {
+      text.css({'font-size': '1px'})
+      var container = text.parent();
+
+      var parSize = container.width();
+      var curSize = text.width();
+      var lastFontSize, newFontSize;
+      var innerRatio = 0.5;
+      while (1 - (curSize / parSize) > 0.05) {
+        lastFontSize = newFontSize;
+        var curFontSize = parseFloat(text.css('font-size'), 10);
+        newFontSize = curFontSize * parSize/curSize * innerRatio;
+        text.css({
+          'font-size': newFontSize + 'px',
+          'line-height': newFontSize + 'px',
+          'overflow': 'visible'
+        });
+        parSize = container.width();
+        curSize = text.width();
+        curHeight = text.height();
+        innerRatio += 0.01;
+        if (settings.maxHeight && curHeight > settings.maxHeight ||
+          1 - (curSize/parSize) < 0) {
+          break;
+        }
+      }
+
       text.css({
-        'font-size': newFontSize + 'px',
-        'line-height': newFontSize + 'px',
+        'font-size': lastFontSize * settings.ratio + 'px',
+        'line-height': lastFontSize * settings.ratio + 'px',
         'overflow': 'visible'
       });
-      parSize = container.width();
-      curSize = text.width();
-      innerRatio += 0.01;
     }
-
-    text.css({
-      'font-size': lastFontSize * settings.ratio + 'px',
-      'line-height': lastFontSize * settings.ratio + 'px',
-      'overflow': 'visible'
-    });
 
   };
 }(jQuery));
